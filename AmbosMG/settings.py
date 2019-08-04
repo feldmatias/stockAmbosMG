12dc81be1d127fb53d6966c4c973551b36153e5e
matias feld <matias.feld@mdevz.com>
2019-08-03 22:47:39 -0300

    debug env variable

--- AmbosMG/settings.py
+++ AmbosMG/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0cl8bl==4rhbaeobnsiwojhgrvp7n95u#a+0_dfals-*2mq96w'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '0cl8bl==4rhbaeobnsiwojhgrvp7n95u#a+0_dfals-*2mq96w')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
