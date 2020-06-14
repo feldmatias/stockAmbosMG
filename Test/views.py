c20988abe64d61cc4e20d4e26b858b9fd2b0ed1f
Matias Feld <feldmatias@gmail.com>
2020-06-14 20:54:03 -0300

    added test for meli notifications

--- Test/views.py
+++ Test/views.py
@@ -2,6 +2,7 @@ import json
 
 from django.http import HttpResponse
 # Create your views here.
+from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
@@ -11,6 +12,10 @@ from Test.models import MeliEvent
 
 @method_decorator(csrf_exempt, name='dispatch')
 class MeliNotificationsView(View):
+
+    def get(self, request, *args, **kwargs):
+        events = MeliEvent.objects.all()
+        return render(request, 'test/config_list.html', {'events': events})
 
 
     def post(self, request, *args, **kwargs):
