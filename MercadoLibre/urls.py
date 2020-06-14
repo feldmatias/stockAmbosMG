3ca0814bb5ea6940fe02bd9a55f88d8794553a71
Matias Feld <feldmatias@gmail.com>
2020-06-14 19:27:11 -0300

    delete meli user

--- MercadoLibre/urls.py
+++ MercadoLibre/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from MercadoLibre.views.AuthorizeMeliUserView import AuthorizeMeliUserView
 from MercadoLibre.views.CreateMeliUserView import CreateMeliUserView
+from MercadoLibre.views.DeleteMeliUserView import DeleteMeliUserView
 from MercadoLibre.views.MapMeliItemsView import MapMeliItemsView
 from MercadoLibre.views.MeliIndexView import MeliIndexView
 
@@ -11,5 +12,6 @@ urlpatterns = [
     path('', MeliIndexView.as_view(), name='index'),
     path('usuarios/nuevo/', CreateMeliUserView.as_view(), name='create_user'),
     path('usuarios/autorizar/', AuthorizeMeliUserView.as_view(), name='authorize_user'),
+    path('usuarios/eliminar/<int:id>', DeleteMeliUserView.as_view(), name='delete_user'),
     path('publicaciones/mapear/<int:user_id>', MapMeliItemsView.as_view(), name='map_items'),
 ]
---
+++ MercadoLibre/views/DeleteMeliUserView.py
@@ -0,0 +1,17 @@
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, redirect
+from django.views import View
+
+from MercadoLibre.models import MeliUser
+
+
+class DeleteMeliUserView(View):
+
+    def post(self, request, *args, **kwargs):
+        user_id = kwargs.get('id')
+        user = get_object_or_404(MeliUser, pk=user_id)
+
+        user.delete()
+        messages.success(request, 'Usuario eliminado con éxito')
+
+        return redirect('mercadolibre:config')
